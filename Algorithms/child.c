0d2baf630e71315f428f9f0913b9752905e82656
Nirmal Ram <36753701+Ramnirmal0@users.noreply.github.com>
2020-06-05 20:53:31 +0530

    Update child.c

--- Algorithms/child.c
+++ Algorithms/child.c
@@ -3,48 +3,51 @@ begin{
 	#include access_key_pair
 	RF24Network network(radio);
 	void activate_detection_coil(){
-		long unsigned int cTime = millis();
-		char buzState = 0;
+		RF24NetworkHeader master_radio(master_node_addrx);
+		define cTime = millis();
+		define buzState = 0;
 		if (cTime < pTime + 10)
 		{
 		  if (diff > 0)
 			buzState = 1;
+			swarm_safety=false;
+			master_safety=false;
+			network.write(master_radio,master_safety);
 		  else if (diff < 0)
 			buzState = 2;
+			swarm_safety=false;
+			master_safety=false;
+			network.write(master_radio,master_safety);
 		}
 		if (cTime > pTime + buzPeriod)
 		{
 		  if (diff > 0)
 			buzState = 1;
+			swarm_safety=false;
+			master_safety=false;
+			network.write(master_radio,master_safety);
 		  else if (diff < 0)
 			buzState = 2;
+			swarm_safety=false;
+			master_safety=false;
+			network.write(master_radio,master_safety);
 		  pTime = cTime;
 		}
 		if (buzPeriod > 300)
-		  buzState = 0;
-		if (buzState == 0)
-		{
-		  noTone(buz);
-		}
-		else if (buzState == 1)
-		{
-		  tone(buz, 2000);
-		}
-		else if (buzState == 2)
-		{
-		  tone(buz, 500);
-		}
+			buzState = 0;
+			swarm_safety=true;
+			master_safety=true;
+			network.write(master_radio,master_safety);
 	}
 	void execute_master_cmd(){
 		define module_setup();
 	}
 	void main{
-		network.update();
-		activate_detection_coil();
 		while(network.avaialable()){
-			Listen_radio();
-			cipher_code=network.read();
+			network.update();
+			swarm_cipher_code=network.read(master_node_addrx);
 			swarm_code=decode(cipher_code,hex_key,access_key_pair);
+			activate_detection_coil();
 		}
 		if(swarm_safe_state==true){
 			if(swarm_code==swarm_cmd){
@@ -52,5 +55,4 @@ begin{
 			}
 		}
 	}
-	
 }
